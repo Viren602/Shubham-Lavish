7840e690c217e35e655ee0baf6d438819521ebe5
Viren <patelvirenb602@gmail.com>
2025-09-02 11:39:27 +0530

    document Js file chnages

--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,8 @@ class MyDocument extends Document {
                     <meta property="og:type" content="website" />
                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOIrigin />
-                    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Quicksand:wght@300..700&display=swap" rel="stylesheet"/>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+                    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Quicksand:wght@300..700&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
