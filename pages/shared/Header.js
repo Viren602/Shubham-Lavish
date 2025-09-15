7b7d7ca057fa791d32888b3f2d6b9c83eb58cadb
Viren <patelvirenb602@gmail.com>
2025-09-15 09:45:52 +0530

    Project setup

--- pages/shared/Header.js
+++ pages/shared/Header.js
@@ -14,37 +14,7 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header">
-        <div className="container">
-          <div className="flex justify-between items-center">
-            <div>
-              <img src="" alt="Logo" className="ls w-[100px] h-[100px]"></img>
-            </div>
-            <div className="flex items-center gap-[50px]">
-              <Link href="/">
-                <a>Home</a>
-              </Link>
-
-              <Link href="/">
-              <a>ABOUT US</a>
-              </Link>
-
-              <Link href="/">
-                <a>SERVICES</a>
-              </Link>
-
-              <Link href="/">
-                <a>BLOG</a>
-              </Link>
-            </div>
-            <div className="flex justify-center items-center py-[11px] px-[25px] border border-black h-fit">
-              <Link href="/" >
-                <a className="flex gap-[10px] items-center">
-                  home <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
-                </a>
-              </Link>
-            </div>
-          </div>
-        </div>
+        
       </div>
     );
   }
