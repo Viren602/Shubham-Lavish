0b401047a0a6f36c51b3cf70da42e9792c607e93
Viren <patelvirenb602@gmail.com>
2025-09-16 09:41:18 +0530

    Home page changes

--- pages/home/Home.js
+++ pages/home/Home.js
@@ -12,186 +12,189 @@ class Home extends Component {
     return (
       <>
         <div className="home-page">
-          
-        <div className="container">
-          <div className="inner flex mt-6 gap-10">
-            <div className="w-1/2">
-              <h1 className="mt-[30px]">
-                Create A Modern And Unique Design Your Dream
-              </h1>
-              <p className="mt-[25px]">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Adipiscing dui tellus commodo convallis. Auctor eget orci
-                pharetra non. Integer lorem in scelerisque tortor dui tempor
-              </p>
-              <button className="bg-black text-white pl-3 pr-3 pt-2 pb-2 mt-8">
-                VIEW A PROJECT
-              </button>
-            </div>
-            <div>
-              <img src="banner.png"></img>
-            </div>
-          </div>
-        </div>
+          <div className="banner">
 
-        <div className="container">
-          <div className="inner mt-[40px] flex items-center gap-[90px]">
-            <div className="text-lg">
-              Trusted by 75+ <br />
-              <span>Partners</span>
-            </div>
-            <div className="">
-              <ul className="flex w-[900px] gap-[35px]">
-                <li>
-                  <img src="Logo-1.png" />
-                </li>
-                <li>
-                  <img src="Logo-2.png" />
-                </li>
-                <li>
-                  <img src="Logo-3.png" />
-                </li>
-                <li>
-                  <img src="Logo-4.png" />
-                </li>
-                <li>
-                  <img src="Logo-5.png" />
-                </li>
-                <li>
-                  <img src="Logo-6.png" />
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="inner">
-            <div className="flex mt-[60px] gap-[50px]">
-              <div className="static flex gap-[25px]">
-                <div>
-                  <img src="frame-image.png" />
+            <div className="container">
+              <div className="inner flex mt-6 gap-10">
+                <div className="w-1/2">
+                  <h1 className="mt-[30px]">
+                    Create A Modern And Unique Design Your Dream
+                  </h1>
+                  <p className="mt-[25px]">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Adipiscing dui tellus commodo convallis. Auctor eget orci
+                    pharetra non. Integer lorem in scelerisque tortor dui tempor
+                  </p>
+                  <button className="bg-black text-white pl-3 pr-3 pt-2 pb-2 mt-8">
+                    VIEW A PROJECT
+                  </button>
                 </div>
                 <div>
-                  <img src="hall-image.png" />
-                  <div className="absolute top-[960px] right-[445px] left-[0px] w-[350px] mx-auto">
-                    <img src="frame-info.png" className="mr-[200px]" />
+                  <img src="banner.png"></img>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="container">
+            <div className="inner mt-[40px] flex items-center gap-[90px]">
+              <div className="text-lg">
+                Trusted by 75+ <br />
+                <span>Partners</span>
+              </div>
+              <div className="">
+                <ul className="flex w-[900px] gap-[35px]">
+                  <li>
+                    <img src="Logo-1.png" />
+                  </li>
+                  <li>
+                    <img src="Logo-2.png" />
+                  </li>
+                  <li>
+                    <img src="Logo-3.png" />
+                  </li>
+                  <li>
+                    <img src="Logo-4.png" />
+                  </li>
+                  <li>
+                    <img src="Logo-5.png" />
+                  </li>
+                  <li>
+                    <img src="Logo-6.png" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="inner">
+              <div className="flex mt-[60px] gap-[50px]">
+                <div className="static flex gap-[25px]">
+                  <div>
+                    <img src="frame-image.png" />
+                  </div>
+                  <div>
+                    <img src="hall-image.png" />
+                    <div className="absolute top-[960px] right-[445px] left-[0px] w-[350px] mx-auto">
+                      <img src="frame-info.png" className="mr-[200px]" />
+                    </div>
+                  </div>
+                </div>
+                ``
+                <div className="w-[680px] mt-[40px]">
+                  <h6 className="fs-[18px]">---- Hello There</h6>
+                  <h1 className="text-[40px]">
+                    We work professionally and wholeheartedly
+                  </h1>
+                  <p className="mt-[20px]">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Adipiscing dui tellus commodo convallis. Auctor eget orci
+                    pharetra non. Integer lorem in scelerisque tortor dui tempor,
+                    volutpat viverra faucibus. Sagittis maecenas tincidunt at
+                    purus. Amet non vel elit aliquet id dolor risus imperdiet
+                    vitae. Amet, sagittis mauris morbi erat velit id nullam. Lorem
+                    ipsum dolor.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="inner mt-[90px]">
+              <h6>Services</h6>
+              <h3 className="text-[40px] mt-[10px]">
+                Our capacity to help make your wishes come true
+              </h3>
+              <div className="mt-[20px]">
+                <ul className="flex">
+                  <li>
+                    <img src="interior design.png" />
+                  </li>
+                  <li>
+                    {" "}
+                    <img src="Architecture.png" />
+                  </li>
+                  <li>
+                    <img src="Furniture Design.png" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="inner mt-[90px]">
+              <h6>Our Project</h6>
+              <h3 className="text-[40px] mt-[10px]">Our latest projects</h3>
+              <div className="static mt-[20px]">
+                <img src="Furniture.png" />
+                <h6 className="absolute top-[2300px] left-[100px] text-white">Hanani Home</h6>
+                <p className="absolute top-[2350px] left-[100px] text-white w-[300px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="testimonial py-[80px]">
+            <div className="container">
+              <div className="inner">
+                <h6>Testimonial</h6>
+                <h4 className="mt-[20px]">What Our Clients Say</h4>
+                <div className="flex gap-[40px] mt-[20px]">
+                  <div className="p-[30px]  bg-gray-100">
+                    <div className="flex gap-[20px]">
+                      <div><img src="Ellipse 1.png" /></div>
+                      <div className="mt-2">
+                        <h5>Menta Nia</h5>
+                        <p>Company Name</p>
+                      </div>
+                    </div>
+                    <p className="mt-[25px]">“ Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non “
+                    </p>
+                  </div>
+                  <div className="p-[30px] bg-gray-100">
+                    <div className="flex gap-[20px]">
+                      <div><img src="Ellipse 1.png" /></div>
+                      <div className="mt-2">
+                        <h5>Menta Nia</h5>
+                        <p>Company Name</p>
+                      </div>
+                    </div>
+                    <p className="mt-[25px]">“ Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non “
+                    </p>
+                  </div>
+                  <div className="p-[30px] bg-gray-100">
+                    <div className="flex gap-[20px]">
+                      <div><img src="Ellipse 1.png" /></div>
+                      <div className="mt-2">
+                        <h5>Menta Nia</h5>
+                        <p>Company Name</p>
+                      </div>
+                    </div>
+                    <p className="mt-[25px]">“ Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non “
+                    </p>
                   </div>
                 </div>
               </div>
-              ``
-              <div className="w-[680px] mt-[40px]">
-                <h6 className="fs-[18px]">---- Hello There</h6>
-                <h1 className="text-[40px]">
-                  We work professionally and wholeheartedly
-                </h1>
-                <p className="mt-[20px]">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Adipiscing dui tellus commodo convallis. Auctor eget orci
-                  pharetra non. Integer lorem in scelerisque tortor dui tempor,
-                  volutpat viverra faucibus. Sagittis maecenas tincidunt at
-                  purus. Amet non vel elit aliquet id dolor risus imperdiet
-                  vitae. Amet, sagittis mauris morbi erat velit id nullam. Lorem
-                  ipsum dolor.
-                </p>
+            </div>
+          </div>
+
+          <div className="cta bg-[url('/CTC.png')] bg-cover bg-center bg-no-repeat py-[80px]">
+            <div className="container w-full mx-auto">
+              <div className="cta-inner w-full w-[40%]">
+                <h4>Have A Question ? Start Consultation Now</h4>
+                <p className="cta-inner mt-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing dui tellus commodo convallis.</p>
+                <button className="bg-black text-white pl-[10px] pr-[10px] pt-[5px] pb-[5px] mt-[20px]">CONTACT US</button>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="container">
-          <div className="inner mt-[90px]">
-            <h6>Services</h6>
-            <h3 className="text-[40px] mt-[10px]">
-              Our capacity to help make your wishes come true
-            </h3>
-            <div className="mt-[20px]">
-              <ul className="flex">
-                <li>
-                  <img src="interior design.png" />
-                </li>
-                <li>
-                  {" "}
-                  <img src="Architecture.png" />
-                </li>
-                <li>
-                  <img src="Furniture Design.png" />
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="inner mt-[90px]">
-            <h6>Our Project</h6>
-            <h3 className="text-[40px] mt-[10px]">Our latest projects</h3>
-            <div className="static mt-[20px]">
-              <img src="Furniture.png" />
-              <h6 className="absolute top-[2300px] left-[100px] text-white">Hanani Home</h6>
-              <p className="absolute top-[2350px] left-[100px] text-white w-[300px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non.</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="testimonial py-[80px]">
-        <div className="container">
-          <div className="inner">
-            <h6>Testimonial</h6>
-            <h4 className="mt-[20px]">What Our Clients Say</h4>
-          <div className="flex gap-[40px] mt-[20px]">
-            <div className="p-[30px]  bg-gray-100">
-              <div className="flex gap-[20px]">
-                <div><img src="Ellipse 1.png"/></div>
-                  <div className="mt-2">
-                    <h5>Menta Nia</h5>
-                    <p>Company Name</p>
-                  </div>
-              </div>
-              <p className="mt-[25px]">“ Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non “
-              </p>
-            </div>
-            <div className="p-[30px] bg-gray-100">
-              <div className="flex gap-[20px]">
-                <div><img src="Ellipse 1.png"/></div>
-                  <div className="mt-2">
-                    <h5>Menta Nia</h5>
-                    <p>Company Name</p>
-                  </div>
-              </div>
-              <p className="mt-[25px]">“ Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non “
-              </p>
-            </div>
-            <div className="p-[30px] bg-gray-100">
-              <div className="flex gap-[20px]">
-                <div><img src="Ellipse 1.png"/></div>
-                  <div className="mt-2">
-                    <h5>Menta Nia</h5>
-                    <p>Company Name</p>
-                  </div>
-              </div>
-              <p className="mt-[25px]">“ Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non Adipiscing dui tellus commodo convallis. Auctor eget orci pharetra non “
-              </p>
-            </div>
-          </div>
-          </div>
-        </div>
-        </div>
-
-        <div className="cta bg-[url('/CTC.png')] bg-cover bg-center bg-no-repeat py-[80px]">
-          <div className="container w-full mx-auto">
-            <div className="cta-inner w-full w-[40%]">
-              <h4>Have A Question ? Start Consultation Now</h4>
-              <p className="cta-inner mt-[20px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing dui tellus commodo convallis.</p>
-              <button className="bg-black text-white pl-[10px] pr-[10px] pt-[5px] pb-[5px] mt-[20px]">CONTACT US</button>
-            </div>
-          </div>
-        </div>
-
-        <div className="container">
-          <div className="inner mt-[90px]">
-            <h6>Our Blog</h6>
-            <h3 className="mt-[10px]">Latest News and Articles</h3>
+          <div className="container">
+            <div className="inner mt-[90px]">
+              <h6>Our Blog</h6>
+              <h3 className="mt-[10px]">Latest News and Articles</h3>
               <div className="mt-[20px] flex gap-[40px]">
                 <div>
                   <img src="Design1.png" />
